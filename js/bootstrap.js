d5e03222b649a9909927d90e73cbe32a024c998c
Michael Pelletier <michael@societyofgrownups.com>
2015-12-29 12:20:56 -0500

    Initial Commit.

---
+++ js/bootstrap.js
@@ -0,0 +1,6 @@
+var Portfolio = Portfolio || {}
+
+Portfolio = angular.module('portfolio_app', ['portfolio_app.controllers', 'portfolio_app.directives']);
+
+Portfolio.Controllers = angular.module('portfolio_app.controllers', []);
+Portfolio.Directives = angular.module('portfolio_app.directives', []);
